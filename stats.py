c1722857aba99b705dbde921e3cbee825d3181e2
Matthew Sutton <msutton2@wisc.edu>
2015-08-27 14:08:36 -0500

    revised stats and test stats

--- stats.py
+++ stats.py
@@ -1,16 +1,5 @@
 def mean(vals):
-	total = sum(vals)
+	total = float(sum(vals))
 	length = len(vals)
 	return total/length
-
-def test_mean():
-	assert(mean([2,4]) == 3)
-test_mean()
-
-def test_float_mean():
-	assert(mean([2,4]) ==1.5
-test_float_mean()
-
-
-# print mean ([2,4])
 
--- test_stats.py
+++ test_stats.py
@@ -1,15 +1,15 @@
 from stats import mean
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equal
 
 def test_mean():
-	assert(mean([2,4]), 3)
+	assert_equal(mean([2,4]), 3)
 #test_mean()
 
 def test_float_mean():
-	assert(mean([1,2]) == 1.5)
+	assert_equal(mean([1.0,2]), 1.5)
 #test-float_mean()
 
 def test_neg_mean():
-	assert(mean([-2,2,4]) == 1.333)
+	assert_almost_equal(mean([-2,2,4]), 1.333, places=3)
 #test_neg_mean()
 
